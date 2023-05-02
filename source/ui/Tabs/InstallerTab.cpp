9a1b4bb831e6818081f9118b8ea662605950691b
Raúl Álvarez <raulwii2@gmail.com>
2023-05-02 19:29:37 +0200

    Toda la tarde para sacar una ventana? jaja asies

--- source/ui/Tabs/InstallerTab.cpp
+++ source/ui/Tabs/InstallerTab.cpp
@@ -6,9 +6,19 @@ namespace Mara::ui {
     InstallerTab::InstallerTab() {
         tablist = new brls::List();
         if(!pm::isInApplicationMode()){
+
+            char path[255];
+            for (auto &title : Mara::ns::getAllTitles())
+            {
+                if(title.second->GetTitleID() == GAME_PID_USA || title.second->GetTitleID() == GAME_PID_EUR) {
+                    sprintf(path, "sdmc:/atmosphere/contents/%016lX/", title.second->GetTitleID());
+                    break;
+                }
+            }
+
             brls::ListItem* appletinstall = new brls::ListItem("main/installer/appletinstallwarning/btntitle"_i18n,
                                                                "main/installer/appletinstallwarning/btnsubtitle"_i18n);
-            appletinstall->getClickEvent()->subscribe([](brls::View *view) {
+            appletinstall->getClickEvent()->subscribe([path](brls::View *view) {
                 brls::Logger::debug("Instalando arrancador..");
                 brls::Dialog* dialog = new brls::Dialog("main/installer/appletinstallwarning/message"_i18n);
 
@@ -16,16 +26,7 @@ namespace Mara::ui {
                     dialog->close();
                 };
 
-                brls::GenericEvent::Callback yesCallback = [dialog](brls::View* view) {
-                    char path[255];
-                    for (auto &title : Mara::ns::getAllTitles())
-                    {
-                        if(title.second->GetTitleID() == GAME_PID_USA || title.second->GetTitleID() == GAME_PID_EUR) {
-                            sprintf(path, "sdmc:/atmosphere/contents/%016lX/", title.second->GetTitleID());
-                            break;
-                        }
-                    }
-
+                brls::GenericEvent::Callback yesCallback = [dialog, path](brls::View* view) {
                     Result rc = Mara::fs::createdir("sdmc:/atmosphere/contents/");
                     if(R_SUCCEEDED(rc)){
                         Mara::fs::createdir(path);
@@ -34,28 +35,32 @@ namespace Mara::ui {
                     std::string nspgame = "exefs.nsp";
                     std::string nsppath = path + nspgame;
                     if(Mara::fs::copy_file(HBL_GAME, nsppath.c_str())) {
-                        //brls::Application::notify("En el proximo arranque del juego lanzará automaticamente el parcheador.");
-                        auto* dialog_done = new brls::Dialog("main/installer/appletinstalldone/message"_i18n);
+                        /*brls::Application::notify("En el proximo arranque del juego lanzará automaticamente el parcheador.");
+                        dialog->close();*/
+                        dialog->hide([dialog]() {
+                            brls::Dialog* dialog_done = new brls::Dialog("main/installer/appletinstalldone/message"_i18n);
 
-                        brls::GenericEvent::Callback yesCallback_done = [dialog_done](brls::View* view){
-                            for (auto &title : Mara::ns::getAllTitles())
-                            {
-                                if(title.second->GetTitleID() == GAME_PID_USA || title.second->GetTitleID() == GAME_PID_EUR) {
-                                    title.second->Launch();
-                                    break;
+                            brls::GenericEvent::Callback yesCallback_done = [dialog_done](brls::View* view){
+                                for (auto &title : Mara::ns::getAllTitles())
+                                {
+                                    if(title.second->GetTitleID() == GAME_PID_USA || title.second->GetTitleID() == GAME_PID_EUR) {
+                                        title.second->Launch();
+                                        brls::Application::quit();
+                                        break;
+                                    }
                                 }
-                            }
-                        };
+                            };
 
-                        brls::GenericEvent::Callback noCallback_done = [dialog_done](brls::View* view) {
-                            dialog_done->close();
-                        };
+                            brls::GenericEvent::Callback noCallback_done = [dialog_done, dialog](brls::View* view) {
+                                dialog_done->close();
+                            };
 
-                        dialog_done->addButton("main/installer/appletinstalldone/yes"_i18n, yesCallback_done);
-                        dialog_done->addButton("main/installer/appletinstalldone/no"_i18n, noCallback_done);
+                            dialog_done->addButton("main/installer/appletinstalldone/yes"_i18n, yesCallback_done);
+                            dialog_done->addButton("main/installer/appletinstalldone/no"_i18n, noCallback_done);
+                            dialog_done->setCancelable(true);
+                            dialog_done->open();
+                        });
                     }
-
-                    dialog->close();
                 };
 
                 dialog->addButton("main/installer/appletinstallwarning/yes"_i18n, yesCallback);
@@ -64,7 +69,31 @@ namespace Mara::ui {
                 dialog->setCancelable(true);
                 dialog->open();
             });
+
+            auto* appletlaunch = new brls::ListItem("Lanzar Juego", "");
+            appletlaunch->getClickEvent()->subscribe([path](brls::View *view) {
+                brls::Logger::debug("Lanzado juego...");
+
+
+                std::string nspgame = "exefs.nsp";
+                std::string nsppath = path + nspgame;
+
+                if(R_SUCCEEDED(Mara::fs::checkFile(nsppath.c_str()))){
+                    for (auto &title : Mara::ns::getAllTitles())
+                    {
+                        if(title.second->GetTitleID() == GAME_PID_USA || title.second->GetTitleID() == GAME_PID_EUR) {
+                            title.second->Launch();
+                            brls::Application::quit();
+                            break;
+                        }
+                    }
+                } else {
+                    brls::Application::notify("No has instalado todavía el parcheador, instala el parcheador para poder lanzar el juego desde Mara.");
+                }
+            });
+
             tablist->addView(appletinstall);
+            tablist->addView(appletlaunch);
         }
     }
 
